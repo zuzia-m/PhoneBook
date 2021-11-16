e3280f5f501feb87e81b49d76c9b7f838026be31
Zuzia Macińska <zuziazla91@gmail.com>
2021-11-16 15:47:11 +0100

    Add project files.

---
+++ PhoneBook/PhoneBook.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class PhoneBook
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+        private void SetForegroundColor(ConsoleColor color, string text)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        private void DisplayContactDetails(Contact contact)
+        {
+            SetForegroundColor(ConsoleColor.Green, $"Contact: {contact.Name}, {contact.Number}");
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = Contacts.FirstOrDefault(x => x.Number == number);
+
+            if (contact == null)
+            {
+                SetForegroundColor(ConsoleColor.Red, "Contact not found");
+            }
+            else
+                DisplayContactDetails(contact);
+        }
+
+      
+
+        private void DisplayContactsDetails(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayAllContacts()
+        {
+            bool phonebookIsNotEmpty = Contacts.Any();
+            if (!phonebookIsNotEmpty)
+                SetForegroundColor(ConsoleColor.Red, "There's no contacts in the phonebook.");
+            else
+            {
+                var contsctsOrderedByName = Contacts
+                .OrderBy(x => x.Name).ToList();
+                DisplayContactsDetails(contsctsOrderedByName);
+            }
+
+        }
+
+        public void DisplayMatchingContacts(string searchPharse)
+        {
+            var matchingContacts = Contacts
+                .Where(x => x.Name.ToLower().Contains(searchPharse.ToLower()))
+                .OrderBy(x => x.Name).ToList();
+            if (matchingContacts.Any())
+                DisplayContactsDetails(matchingContacts);
+            else
+                SetForegroundColor(ConsoleColor.Red, "There's no matching contacts.");
+        }
+        public void DeleteByNumber(string number)
+        {
+            var contactToRemove = Contacts.FirstOrDefault(x => x.Number == number);
+            if(contactToRemove != null)
+            {
+                Contacts.Remove(contactToRemove);
+                SetForegroundColor(ConsoleColor.Green, $"Contact {contactToRemove.Name}, {contactToRemove.Number} has been succesfully removed.");
+            }
+            else
+            {
+                SetForegroundColor(ConsoleColor.Red, $"Contact with number {number} doesnt exist in the PhoneBook.");
+            }
+        }
+    }
+}
---
+++ PhoneBook/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SetColor(ConsoleColor.Magenta, "Hello to the PhoneBook App!");
+            bool CloseApp = false;
+            var phonebook = new PhoneBook();
+            while (!CloseApp)
+            {
+                Console.WriteLine();
+                SetColor(ConsoleColor.Cyan, 
+                    "1 - Add contact\n" +
+                    "2 - Display contact by number\n" +
+                    "3 - Display all contacts\n" +
+                    "4 - Search contacts\n" +
+                    "5 - Remove contact\n" +
+                    "X - Close app\n");
+
+                SetColor(ConsoleColor.Yellow, "What you want to do? \nPress key 1, 2, 3, 4, 5 or X: ");
+                var userInput = Console.ReadLine().ToUpper();
+
+                switch (userInput)
+                {
+                    case "1":
+                        SetColor(ConsoleColor.Yellow, "Insert Number: ");
+                        var numberInput = Console.ReadLine();
+                       
+                        bool IsDigitsOnly(string number)
+                        {
+                            foreach (char c in number)
+                            {
+                                if (c < '0' || c > '9')
+                                    return false;
+                            }
+                            return true;
+                        }
+                       
+                        string number = null;
+                        
+
+                        if (numberInput.Length >= 9 && IsDigitsOnly(numberInput))
+                            number = numberInput;
+                        else
+                        {
+                            SetColor(ConsoleColor.Red, "The number has to contains only digits and the minimum length is 9.");
+
+                            break;
+                        }
+
+                        SetColor(ConsoleColor.Yellow, "Insert Name: ");
+                        var nameInput = Console.ReadLine();
+                        string name = null;
+                        if (nameInput.Length >= 3)
+                            name = nameInput;
+                        else
+                        {
+                            SetColor(ConsoleColor.Red, "Minimum length of name is 3 chars. Please try again.");
+                            break;
+                        }
+
+                        var newContact = new Contact(name, number);
+
+                        phonebook.AddContact(newContact);
+                        break;
+
+                    case "2":
+                        SetColor(ConsoleColor.Yellow, "Insert Number: ");
+                        var numberToSearch = Console.ReadLine();
+                        phonebook.DisplayContact(numberToSearch);
+                        break;
+                    case "3":
+                        phonebook.DisplayAllContacts();
+                        
+                        break;
+                    case "4":
+                        SetColor(ConsoleColor.Yellow, "Insert Name: ");
+                        var nameToSearch = Console.ReadLine();
+                        phonebook.DisplayMatchingContacts(nameToSearch);
+                        break;
+                    case "5":
+                        SetColor(ConsoleColor.Yellow, "Insert Number to delete: ");
+                        var numberToDelete = Console.ReadLine();
+                        phonebook.DeleteByNumber(numberToDelete);
+                        break;
+                    case "X":
+                        CloseApp = true;
+
+                        break;
+                    default:
+                        SetColor(ConsoleColor.Red, "Invalid operation.\n");
+                        continue;
+                }
+
+                //Console.ForegroundColor = ConsoleColor.Yellow;
+                //Console.WriteLine("\n\n1 - Main Menu\nX or any other - Close the App");
+                //Console.ResetColor();
+                //var userInput2 = Console.ReadLine();
+                //if (userInput2 == "1")
+                //    CloseApp = false;
+                //else
+                //    CloseApp = true;
+                //Console.Clear();
+            }
+
+        }
+
+        private static void SetColor(ConsoleColor color, string text)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
